79bc9058c8b6161ae52fee8b08d23d8dba74e78d
cresent-luna <sandhiyakomala996@gmail.com>
2026-06-16 16:59:02 +0530

    Task-1

---
+++ Task1/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function(){
+
+    let navbar =
+    document.getElementById("navbar");
+
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }
+    else{
+        navbar.classList.remove("scrolled");
+    }
+
+});
